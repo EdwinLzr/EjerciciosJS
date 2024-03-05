5be256fee9a4cb2fdbd4a8cf9f8590e4838d1f0d
Edwin Loaiza <edlr19827030@gmail.com>
2024-03-05 10:34:44 +0100

    operadores

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,26 @@
+// operadores
+
+let numero1=2;
+let numero2=5;
+
+// mayor que
+numero2 > numero1;
+// menor que
+numero2 < numero1;
+
+//========= COMPARACION==================
+
+// asignacion 
+numero2=6
+// comparacion basica
+numero2 == numero1;
+// comparacion extricta compara contenido y tipo de dato
+numero2 === numero1;
+
+// TYPO DE DATO
+typeof(numero2)
+
+// comparar si son diferentes
+numero2 != numero1;
+// extricto
+numero2 !== numero1;
