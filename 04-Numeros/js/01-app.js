27e2869df5966f95261acff6c579b6640bfd8ff6
Edwin Loaiza <edlr19827030@gmail.com>
2024-03-05 10:21:58 +0100

    numeros

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,69 @@
+
+// nueros
+const numero1=10
+const precio=200
+const edad= 42
+let resultado;
+
+
+const  nuemros= new Number(10)
+
+// operaciones basicas con numeros
+
+resultado=numero1 + precio;
+resultado=numero1 - precio;
+resultado=numero1 * precio;
+resultado=numero1 / precio;
+resultado=numero1 % precio;
+
+// operaciones con el objeto math
+// potenciacion
+Math.pow(edad)
+// pi
+Math.PI
+// raiz cuadrada
+Math.sqrt(precio)
+// REDONDEAR NUMEROS hacia arriba
+Math.ceil(12.5)
+// redondear hacia abajo
+Math.floor(2.5)
+// redondear hacia arriba
+Math.round(2.6)
+// valor absoluto
+Math.abs(12)
+// valor minimo de un rango
+Math.min(1, 6, 8)
+// valor maximo de un rango
+Math.max(12, 4, 6)
+// numeros del aleatorios
+Math.floor(Math.random() * 30)
+
+// orden gerarquico de las operaciones
+// funciona igual que en el colegio primero las operaciones en () o con una especificacion en particular
+
+
+// incememntos y decrementos
+
+let puntaje=5;
+
+// incrementar desde el inicio
+++puntaje;
+// decrementar desde el inicio
+--puntaje;
+// incrementarle un valor en especifico
+puntaje +=8;
+// decrementar un valor en especifico
+puntaje-=8;
+
+// methods para numeros
+
+// string
+puntaje='5'
+
+// tipo de dato
+typeof(puntaje)
+// convertir 
+parseInt(puntaje)
+parseFloat(puntaje)
+// verificar si es entero
+Number.isInteger(puntaje)
